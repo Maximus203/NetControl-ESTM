0b84357b671d5337ad1b3a7afce186e07c98321c
printf_cherif <printf0cherif@gmail.com>
2024-10-27 23:29:18 +0000

    Installation des composants Flowbite, Breeze, Livewire Class API. Mise en place des model, migration et seeder pour Role.

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
+        'phone',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -44,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
     }
 }
