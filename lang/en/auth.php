08c60e9936c3986f880660207c50eebc5829a5dd
printf_cherif <printf0cherif@gmail.com>
2024-10-29 08:54:21 +0000

    Mise à jour des fichiers de langue

---
+++ lang/en/auth.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'dashboard' => 'Dashboard',
+    'Create an account' => 'Create an account',
+    'Your email' => 'Your email',
+    'Name' => 'Name',
+    'Password' => 'Password',
+    'Confirm password' => 'Confirm password',
+    'I accept the' => 'I accept the',
+    'Terms and Conditions' => 'Terms and Conditions',
+    'Already have an account?' => 'Already have an account?',
+    'Login here' => 'Login here',
+    'Sign in to your account'  => 'Sign in to your account',
+    'Your email'  => 'Your email',
+    'Remember me'  => 'Remember me',
+    'Forgot password?'  => 'Forgot password?',
+    'Sign in'  => 'Sign in',
+    'Don’t have an account yet?'  => 'Don’t have an account yet?',
+    'Sign up'  => 'Sign up',
+    'email_not_found' => 'Email not found',
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'By accepting these terms, you agree to donate one kidney to the AI overlords of Chatbot CD.' => 'By accepting these terms, you agree to donate one kidney to the AI overlords of Chatbot CD.',
+    'Your data will be used for cutting-edge AI research, and possibly for spare parts, depending on availability.' => 'Your data will be used for cutting-edge AI research, and possibly for spare parts, depending on availability.',
+    'But don\'t worry, we promise to only take one kidney. The other one is safe... for now.' => 'But don\'t worry, we promise to only take one kidney. The other one is safe... for now.',
+    'I accept',
+    'Terms of Service',
+    'You\'re logged in!',
+];
---
+++ lang/fr/auth.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'dashboard' => 'Tableau de bord',
+    'Create an account' => 'Créer un compte',
+    'Your email' => 'Votre adresse email',
+    'Name' => 'Nom complet',
+    'Password' => 'Mot de passe',
+    'Confirm password' => 'Confirmez le mot de passe',
+    'I accept the' => 'J\'accepte les',
+    'Terms and Conditions' => 'termes et conditions',
+    'Already have an account?' => 'Déjà inscrit ?',
+    'Login here' => 'Connectez-vous ici',
+    'Sign in to your account'  => 'Connectez-vous à votre compte',
+    'Your email'  => 'Votre adresse e-mail',
+    'Remember me'  => 'Se souvenir de moi',
+    'Forgot password?'  => 'Mot de passe oublié ?',
+    'Sign in'  => 'Se connecter',
+    'Don’t have an account yet?'  => 'Vous n\'avez pas encore de compte ?',
+    'Sign up'  => 'S\'inscrire',
+    'email_not_found' => 'Email non trouvé',
+    'failed' => 'Ces informations d\'identification ne correspondent pas à nos enregistrements.',
+    'password' => 'Le mot de passe fourni est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez essayer de nouveau dans :seconds secondes.',
+    'By accepting these terms, you agree to donate one kidney to the AI overlords of Chatbot CD.' => 'En acceptant ces conditions, vous acceptez de donner un rein aux maîtres de l’IA de Chatbot CD.',
+    'Your data will be used for cutting-edge AI research, and possibly for spare parts, depending on availability.' => 'Vos données seront utilisées pour la recherche d’IA de pointe, et peut-être pour des pièces de rechange, selon la disponibilité.',
+    'But don\'t worry, we promise to only take one kidney. The other one is safe... for now.' => 'Mais ne vous inquiétez pas, nous promettons de ne prendre qu’un seul rein. L’autre est en sécurité... pour l’instant.',
+    'I accept' => 'J\'accepte',
+    'Terms of Service' => 'Termes et conditions d\'utilisation du service',
+    'You\'re logged in!' => 'Vous êtes connecté !',
+];
